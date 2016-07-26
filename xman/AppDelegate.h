61395f2fe21e811821dc6ccdb547a93509ae1cb3
Liu Ming <liuming@richs-macbook-pro.local>
2016-07-26 15:39:06 +1200

    Initial Commit

---
+++ xman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xman
+//
+//  Created by Liu Ming on 26/07/16.
+//  Copyright © 2016 Liu Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
